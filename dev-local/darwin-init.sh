23bae0cfea34dff39b0dd986b5980c7bfa514b17
Roger Hu <roger.hu@gmail.com>
2023-08-05 07:03:12 +0000

    Bump to Android SDK 33 for Darwin too

--- dev-local/darwin-init.sh
+++ dev-local/darwin-init.sh
@@ -11,7 +11,7 @@ fi
 xcode-select --install || true
 pushd $AOSP_SRC_ROOT
 ln -sf $HOME/Library/Android/sdk $AOSP_SRC_ROOT
-$HOME/Library/Android/sdk/cmdline-tools/latest/bin/sdkmanager --install "ndk;20.1.5948944" "platforms;android-32" "build-tools;30.0.3"
+$HOME/Library/Android/sdk/cmdline-tools/latest/bin/sdkmanager --install "ndk;20.1.5948944" "platforms;android-33" "build-tools;30.0.3"
 # Used to run patches
 export REPO_TOOL="$AOSP_SRC_ROOT/repo"
 # Need to run bazel
